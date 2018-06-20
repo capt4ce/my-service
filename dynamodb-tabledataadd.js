76d2bc9472e9a9f96e456fa5e3d3edae85c0f46e
awe777 <joshuawe777@gmail.com>
2018-06-20 15:40:53 +0800

    added capabilities to add data to table

---
+++ dynamodb-tabledataadd.js
@@ -0,0 +1,60 @@
+'use strict'
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: "ap-southeast-1",
+    endpoint: "http://localhost:8000"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event, context) => {
+    const now = Date.now();
+    if(!event.queryStringParameters) {
+        return fourhundred("No query found.");
+    }
+    if(!event.queryStringParameters.title || !event.queryStringParameters.content) {
+        return fourhundred("Title and/or content missing.");
+    }
+    const params = {
+        TableName: "News",
+        Item: {
+            "dateFloor": Math.floor(now / 850000000),
+            "dateExact": now,
+            "tag": event.queryStringParameters.tag || [],         // diharapkan keluaran array, protection available
+            "title": event.queryStringParameters.title,
+            "content": event.queryStringParameters.content,
+            "image": event.queryStringParameters.image || "",     
+        }
+    }
+    try {
+        const addData = await docClient.put(params).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: `News of "${event.queryStringParameters.title}" on the ${params.TableName} table successfully added with timestamp ${now}.`,
+            }),
+        }
+    } catch(err) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: `${err}`,
+            }),
+        }
+    }
+}
+
+function fourhundred(errmes) {
+    return {
+        statusCode: 400,
+        error: "Bad Request",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            message: errmes + "",
+        }),
+    }
+}
