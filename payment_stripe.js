d0962c07c43e02d6348641a49de211129aa32926
awe777 <joshuawe777@gmail.com>
2018-06-25 15:58:53 +0800

    mistake fix

--- payment_stripe.js
+++ payment_stripe.js
@@ -6,9 +6,6 @@ const queryString = require("querystring");
 exports.handler = async (event, context) => {
     let stripeCurrent = new stripe("sk_test_BQokikJOvBiI2HlWgH4olfQ2");
     let params = queryString.parse(event.body)
-    if(params.apiKey) {
-        stripeCurrent = new stripe(params.apiKey)
-    }
     try {
         const charge = await stripeCurrent.charges.create({
             amount: 200,
