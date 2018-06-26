054cfea07a0ee4de659572a6646019c3b3249bc5
awe777 <joshuawe777@gmail.com>
2018-06-26 17:14:27 +0800

    simplifies Braintree API calls, adds oldUser & newUser calls

--- payment_braintree_checkout.js
+++ payment_braintree_checkout.js
@@ -1,6 +1,13 @@
 'use strict'
+const braintree = require("braintree");
 
-const gateway = require('./lib/gateway.js');
+require('dotenv').load();
+const gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BT_MERCHANT_ID,
+  publicKey: process.env.BT_PUBLIC_KEY,
+  privateKey: process.env.BT_PRIVATE_KEY
+});
 
 exports.handler = async (event, context) => {
   console.log(event)
@@ -11,7 +18,10 @@ exports.handler = async (event, context) => {
     amount: Math.floor(2000 + 2000 * Math.random()), // 50-50 success/fail
     paymentMethodNonce: payload.nonce,
     options: {
-      submitForSettlement: true
+      submitForSettlement: true,
+      threeDSecure: {
+        required: false,
+      },
     }
   });
 
--- payment_braintree_createToken.js
+++ payment_braintree_createToken.js
@@ -12,12 +12,47 @@ exports.handler = async (event, context) => {
         publicKey: process.env.BT_PUBLIC_KEY,
         privateKey: process.env.BT_PRIVATE_KEY
       });
+
+      let newCustomerResult, newCustomerCreation;
+      if(event.body) {
+        newCustomerResult = await gateway.customer.find(JSON.parse(event.body).oldCustomerID + ""); // harus string
+        if(!newCustomerResult) {
+          newCustomerCreation = await gateway.customer.create({});
+          newCustomerResult = await gateway.customer.find(newCustomerCreation.customer.id);
+        }
+      } else {
+        newCustomerCreation = await gateway.customer.create({});
+        console.log(newCustomerCreation.customer.id)
+        newCustomerResult = await gateway.customer.find(newCustomerCreation.customer.id);
+      }
       
+      /**
+       * Alur kerja program ini:
+       * 1. a. Jika dipanggil dari oldUser, maka mencoba cari customer dengan customer.id yang
+       *       ditentukan page tersebut; jika tidak ada, maka buat data customer baru.
+       *    b. Jika dipanggil dari newUser, maka membuat user baru, lalu mencari user tersebut
+       *        - Tindakan ini dilakukan karena instance Dropin yang dipanggil di web page
+       *          hanya melakukan credit check validation pada customer dimana customer.id-nya
+       *          dipanggil dari gateway.customer.find() 
+       * 3. Ambil customer yang ditemukan pencarian
+       * 4. Buat instance Braintree.dropin
+       * 5. Request cara pembayaran: jika user adalah customer lama maka pembayaran 
+       *    sebelumnya akan tertera di instance tersebut
+       * 6. Jika cara pembayaran belum ada, maka user harus memasukkan data pembayaran
+       * 7. Dropin akan melakukan verifikasi terhadap kartu pembayaran yang telah dimasukkan,
+       *    jika kartu tidak valid (https://developers.braintreepayments.com/reference/general/testing/node)
+       *    maka akan ada notifikasi dari instance
+       * 8. Jika kartu valid, akan diteruskan pada checkout
+       * 9. Checkout akan melakukan suatu pembayaran yang nilainya berkisar dari 2000 ke 4000
+       * 10. Jika nilai < 3000 maka transaksi akan gagal
+       */
+
       const resp = await gateway.clientToken.generate({
-          customerId: 558971619,          // untuk sementara
-          merchantAccountId: 'r6nh29dk3z8xykk9',   // untuk sementara
+          customerId: newCustomerResult.id,
+          merchantAccountId: 'r6nh29dk3z8xykk9',    
       });
       
+
       return {
         statusCode: 200,
         body: JSON.stringify(resp)
